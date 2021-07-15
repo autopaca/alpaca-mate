bc87c82c5fbcecb9f5ba1419b940447e1b5fd9fd
autopaca <macopassyrb63@gmail.com>
2021-07-15 23:59:32 +0800

    update styles

--- src/Calculator/index.tsx
+++ src/Calculator/index.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import poolMetas from "./poolMetas";
-import {Button, Checkbox, Image, Input, Select, Card, Spin, Row, Col, Space} from 'antd';
+import {Image, Input, Select, Card, Spin, Row, Col, Space, Slider} from 'antd';
 import "./index.css";
 
 const {Option} = Select;
@@ -74,14 +74,25 @@ function Calculator() {
         <div className={"Calculator"}>
             <Spin spinning={false}>
                 <Card className={"rounded-3xl"}>
-                    <div className={"mt-1 lg:mt-4 lg:mx-7 text-base lg:text-2xl font-bold"}>
-                        <span>Farm</span>
-                        <Select className={"mx-4 w-64"} onChange={choosePool}>
-                            {poolMetas.map((meta, i) => (
-                                <Option value={meta.name} key={i}>{meta.name}</Option>
-                            ))}
-                        </Select>
-                        <span>pool</span>
+                    <div className={"mt-1 lg:mt-4 lg:mx-7 text-base lg:text-2xl font-bold FarmChoosePosition"}>
+                        <Row align={"middle"}>
+                            <Col span={10}>
+                                <span>Which pool would like to farm?</span>
+                            </Col>
+                            <Col>
+                                <Select className={"c-poolSelector mx-4 w-64"} onChange={choosePool}>
+                                    {poolMetas.map((meta, i) => (
+                                        <Option value={meta.name} key={`farm-${meta.name}`}>
+                                            <Row align={"middle"}>
+                                                <Col>
+                                                    {meta.name}
+                                                </Col>
+                                            </Row>
+                                        </Option>
+                                    ))}
+                                </Select>
+                            </Col>
+                        </Row>
                     </div>
                     <Row className={"mt-6 lg:mt-8 text-sm lg:text-xl lg:mx-7"}>
                         <Col span={24}>
@@ -100,13 +111,52 @@ function Calculator() {
                                                 {getPriceInputRow(assets[1])}
                                             </>
                                             }
-                                            <div className="ant-row mt-6 lg:mt-10 lg:mx-4 text-sm lg:text-xl font-bold"
-                                                 style={{rowGap: "0px"}}>
+                                            <div className="ant-row mt-6 lg:mt-10 lg:mx-4 text-sm lg:text-xl font-bold">
                                                 <div className="ant-col"><span>Leverage</span></div>
                                             </div>
-                                            <Row className={"mt-4 ml-2 lg:mt-7 lg:ml-8 lg:mr-4"} style={{rowGap: "0px"}}>
+                                            <Row className={"mt-4 ml-2 lg:mt-7 lg:ml-8 lg:mr-4"}>
                                                 <Col span={24}>
-
+                                                    <Row justify={"center"} className={"SliderBarInput"}>
+                                                        <Col xs={18} lg={20}>
+                                                            <Slider
+                                                                min={0}
+                                                                max={1}
+                                                                // onChange={this.onChange}
+                                                                // value={typeof inputValue === 'number' ? inputValue : 0}
+                                                                step={0.01}
+                                                            />
+                                                        </Col>
+                                                        <Col offset={1} xs={5} lg={3}>
+                                                            <Input className={"w-full rounded-lg"} suffix={"x"}/>
+                                                        </Col>
+                                                    </Row>
+                                                </Col>
+                                            </Row>
+                                            <Row className={"mt-6 lg:mt-10 lg:mx-4 text-sm lg:text-xl font-bold"}>
+                                                <Col>
+                                                    <span>Which asset would you like to borrow?</span>
+                                                </Col>
+                                            </Row>
+                                            <Row className={"mt-4 lg:mt-7 lg:mx-4"}>
+                                                <Col span={24}>
+                                                    <Select className={"c-assetSelector"}>
+                                                        {assets.map((asset) => (
+                                                            <Option value={asset} key={`borrow-${asset}`}>
+                                                                <Row align={"middle"}>
+                                                                    <Col style={{paddingLeft: "8px", paddingRight: "8px", flex: "0 0 auto"}}>
+                                                                        <img
+                                                                            className={"c-assetSelector__optionList__option__icon"}
+                                                                            src={getCoinUrl(asset)}
+                                                                            style={{height: "2.25rem"}}
+                                                                        />
+                                                                    </Col>
+                                                                    <Col style={{paddingLeft: "8px", paddingRight: "8px", flex: "0 0 auto"}}>
+                                                                        {asset}
+                                                                    </Col>
+                                                                </Row>
+                                                            </Option>
+                                                        ))}
+                                                    </Select>
                                                 </Col>
                                             </Row>
                                         </Card>
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import './index.css'
+import App from './App'
 
 
 ReactDOM.render(
