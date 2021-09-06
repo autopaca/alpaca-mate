5862399af06d416203a9f230b1c0c453c5642dc6
autopaca <macopassyrb63@gmail.com>
2021-09-06 23:31:59 +0800

    add test position history feature

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {QueryClient, QueryClientProvider} from 'react-query'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import 'antd/dist/antd.less'; // or 'antd/dist/antd.less'
-import './index.css'
-import {RecoilRoot} from 'recoil';
-import App from './App'
+import './index.css';
+import { RecoilRoot } from 'recoil';
+import App from './App';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { HashRouter as Router } from 'react-router-dom';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <App/>
-      </RecoilRoot>
+      <Router>
+        <RecoilRoot>
+          <App />
+        </RecoilRoot>
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
