2dfee3cf72343be880243bb93dda1be9013f8cf3
autopaca <macopassyrb63@gmail.com>
2021-11-16 20:09:35 +0800

    chore: use windicss rather than tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import path from 'path';
+import WindiCSS from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [WindiCSS(), reactRefresh()],
   css: {
     preprocessorOptions: {
       less: {
